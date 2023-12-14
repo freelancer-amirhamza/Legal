267a6538de66035cc035134c42b827246774823d
Amir <amirhamza27940@gmail.com>
2023-12-15 02:24:32 +0600

    all problems solve

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector('#menu-btn')
 const sideBtn = document.querySelector('#side-btn')
 const menuBar = document.querySelector('.menu-bar')
+const sideMenu = document.querySelector('.side-menu-bar')
 
 
 menuBtn.onclick = () =>{
@@ -9,5 +10,5 @@ menuBtn.onclick = () =>{
 }
 sideBtn.onclick = () =>{
     sideBtn.classList.toggle('bx-x')
-    menuBar.classList.toggle('side-bar-active')
+    sideMenu.classList.toggle('active-side-menu')
 }
